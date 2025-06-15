652be6789030a7c6f28fc43621618ad93ce0cf27
michaelvanopstal <44727975+michaelvanopstal@users.noreply.github.com>
2025-06-15 17:17:28 +0200

    Update game.js

--- game.js
+++ game.js
@@ -93,9 +93,6 @@ bootBlockImg.src = "boot_block_logo.png";
 const boatImg = new Image();
 boatImg.src = "pointpay_bood.png";
 
-const waterOverlayImg = new Image();
-waterOverlayImg.src = "water1.png";
-
 const doubleBallImg = new Image();
 doubleBallImg.src = "2 balls.png";  // upload dit naar dezelfde map
 
