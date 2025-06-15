ca1fe0e715662cf8b4cc65ec4912ad38fefc7938
michaelvanopstal <44727975+michaelvanopstal@users.noreply.github.com>
2025-06-15 13:12:53 +0200

    Update game.js

--- game.js
+++ game.js
@@ -593,7 +593,7 @@ function draw() {
   if (rightPressed) boatX += boatSpeed;
 
   // Boot wordt opgeduwd door water
-  boatY = waterY - paddleHeight * 1.4;
+  boatY = waterY - paddleHeight * 5;)
   ctx.drawImage(boatImg, boatX, boatY, paddleWidth, paddleHeight * 5);
   ctx.drawImage(waterOverlayImg, 0, waterY, canvas.width, canvas.height - waterY);
 } // ✅ sluit hier pas af
