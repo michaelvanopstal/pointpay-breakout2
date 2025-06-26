7bf74b66ea6d5d0f451f82c067a61a4ecc14bc1c
michaelvanopstal <44727975+michaelvanopstal@users.noreply.github.com>
2025-06-26 19:51:29 +0200

    Update game.js

--- game.js
+++ game.js
@@ -43,8 +43,8 @@ const speedBoostMultiplier = 2;
 balls.push({
   x: canvas.width / 2,
   y: canvas.height - paddleHeight - 10,
-  dx: 5,
-  dy: -5,
+  dx: 6,
+  dy: -6,
   radius: 8,
   isMain: true
 });
@@ -302,7 +302,7 @@ function resetBall() {
     x: paddleX + paddleWidth / 2 - ballRadius,
     y: canvas.height - paddleHeight - ballRadius * 2,
     dx: 0,
-    dy: -5,
+    dy: -6,
     radius: ballRadius,
     isMain: true
   }];
