0d8280a1bcf5c2b1385c6669c4a8b25023642f04
michaelvanopstal <44727975+michaelvanopstal@users.noreply.github.com>
2025-06-26 21:09:01 +0200

    Update game.js

--- game.js
+++ game.js
@@ -699,7 +699,7 @@ balls.forEach((ball, index) => {
   if (ballLaunched) {
     let speedMultiplier = (speedBoostActive && Date.now() - speedBoostStart < speedBoostDuration)
       ? speedBoostMultiplier
-      : 2;
+      : 1;
     ball.x += ball.dx * speedMultiplier;
     ball.y += ball.dy * speedMultiplier;
   } else {
