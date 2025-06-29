70838945b31228b0276ad2539dbf7c67a9240064
michaelvanopstal <44727975+michaelvanopstal@users.noreply.github.com>
2025-06-29 20:01:06 +0200

    Update game.js

--- game.js
+++ game.js
@@ -87,14 +87,14 @@ const bonusBricks = [
 const pxpMap = [
   { col: 0, row: 0 }, { col: 1, row: 0 },     { col: 3, row: 1 },   { col: 6, row: 0 },  { col: 7, row: 0 },  
   { col: 0, row: 1 }, { col: 2, row: 0 },     { col: 4, row: 2 },   { col: 6, row: 1 },  { col: 7, row: 3 },          
-  { col: 0, row: 2 },                         { col: 3, row: 3 },   { col: 6, row: 2 },  { col: 8, row: 0 },           
-  { col: 0, row: 3 }, { col: 2, row: 1 },     { col: 5, row: 3 },   { col: 6, row: 3 },  { col: 8, row: 1 }, 
-  { col: 0, row: 4 },                         { col: 5, row: 1 },   { col: 6, row: 4 },             
-  { col: 0, row: 5 }, { col: 2, row: 2 },                           { col: 6, row: 5 },  { col: 8, row: 2 },  
-                                                                    { col: 6, row: 6 },  { col: 8, row: 3 },  
-  { col: 0, row: 6 }, { col: 2, row: 3 },                           { col: 6, row: 7 },  { col: 8, row: 4 },  
-  { col: 0, row: 7 }, { col: 2, row: 4 },                           { col: 6, row: 8 },                
-  { col: 0, row: 8 },  {col:  1, row:1},                                                               
+  { col: 0, row: 2 }, { col: 2, row: 1 },     { col: 3, row: 3 },   { col: 6, row: 2 },  { col: 8, row: 0 },           
+  { col: 0, row: 3 }, { col: 2, row: 2 },     { col: 5, row: 3 },   { col: 6, row: 3 },  { col: 8, row: 1 }, 
+  { col: 0, row: 4 }, { col: 2, row: 3 },     { col: 5, row: 1 },   { col: 6, row: 4 },  { col: 8, row: 2 },           
+  { col: 0, row: 5 }, { col: 2, row: 4 },                           { col: 6, row: 5 },  { col: 8, row: 3 }, 
+  { col: 0, row: 6 }, { col: 1, row: 1 },                           { col: 6, row: 6 },  { col: 8, row: 4 }, 
+  { col: 0, row: 7 },                                               { col: 6, row: 7 },     
+  { col: 0, row: 8 },                                               { col: 6, row: 8 },
+                                                                  
 ];
 
 
