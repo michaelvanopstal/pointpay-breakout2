9139cb30197039fa032b02cb520566da23be776d
michaelvanopstal <44727975+michaelvanopstal@users.noreply.github.com>
2025-06-29 19:47:42 +0200

    Update game.js

--- game.js
+++ game.js
@@ -85,15 +85,15 @@ const bonusBricks = [
 ];
 // 📦 PXP layout voor level 2 (alleen steen-blokken)
 const pxpMap = [
-  { col: 0, row: 0 }, { col: 1, row: 0 },     { col: 3, row: 1 },   { col: 6, row: 0 }  { col: 7, row: 0 }  
-  { col: 0, row: 1 }, { col: 2, row: 0 },     { col: 4, row: 2 },   { col: 6, row: 1 }  { col: 7, row: 3 }          
-  { col: 0, row: 2 },                         { col: 3, row: 3 },   { col: 6, row: 2 }  { col: 8, row: 0 }           
-  { col: 0, row: 3 }, { col: 2, row: 1 },     { col: 5, row: 3 },   { col: 6, row: 3 }  { col: 8, row: 1 }  
-  { col: 0, row: 4 },                         { col: 5, row: 1 },   { col: 6, row: 4 }             
-  { col: 0, row: 5 }, { col: 2, row: 2 },                           { col: 6, row: 5 }  { col: 8, row: 2 }  
-                                                                    { col: 6, row: 6 }  { col: 8, row: 3 }  
-  { col: 0, row: 6 }, { col: 2, row: 3 },                           { col: 6, row: 7 }  { col: 8, row: 4 }  
-  { col: 0, row: 7 }, { col: 2, row: 4 },                           { col: 6, row: 8 }                
+  { col: 0, row: 0 }, { col: 1, row: 0 },     { col: 3, row: 1 },   { col: 6, row: 0 },  { col: 7, row: 0 },  
+  { col: 0, row: 1 }, { col: 2, row: 0 },     { col: 4, row: 2 },   { col: 6, row: 1 },  { col: 7, row: 3 },          
+  { col: 0, row: 2 },                         { col: 3, row: 3 },   { col: 6, row: 2 },  { col: 8, row: 0 },           
+  { col: 0, row: 3 }, { col: 2, row: 1 },     { col: 5, row: 3 },   { col: 6, row: 3 },  { col: 8, row: 1 }, 
+  { col: 0, row: 4 },                         { col: 5, row: 1 },   { col: 6, row: 4 },             
+  { col: 0, row: 5 }, { col: 2, row: 2 },                           { col: 6, row: 5 },  { col: 8, row: 2 },  
+                                                                    { col: 6, row: 6 },  { col: 8, row: 3 },  
+  { col: 0, row: 6 }, { col: 2, row: 3 },                           { col: 6, row: 7 },  { col: 8, row: 4 },  
+  { col: 0, row: 7 }, { col: 2, row: 4 },                           { col: 6, row: 8 },                
   { col: 0, row: 8 },  {col:  1, row:1},                                                               
 ];
 
