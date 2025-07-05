d01ecbbbbc977947135f7dd9b14ac55881238353
michaelvanopstal <44727975+michaelvanopstal@users.noreply.github.com>
2025-07-05 18:30:09 +0200

    Update game.js

--- game.js
+++ game.js
@@ -190,8 +190,6 @@ for (let c = 0; c < brickColumnCount; c++) {
   }
 }
 
-const scoreBoardImg = new Image();
-scoreBoardImg.src = "score.png";
 
 
 const heartBoardImg = new Image();
@@ -1844,7 +1842,6 @@ machinegunGunImg.onload = onImageLoad;
 coinImg.onload = onImageLoad;
 heartImg.onload = onImageLoad; 
 heartBoardImg.onload = onImageLoad;
-scoreImg.onload = onImageLoad;
 
 document.addEventListener("mousedown", function (e) {
   // 🛡️ Alleen reageren als er op het canvas geklikt wordt
